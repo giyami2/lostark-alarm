0b181b0a188c07f00976fa733b012556b7ff57ca
Claire Park <giyami0425@gmail.com>
2023-05-15 01:49:26 +0900

    중요 데이터 환경변수화

--- src/pages/api/alarm.js
+++ src/pages/api/alarm.js
@@ -1,13 +1,10 @@
 import axios from "axios";
-const WEBHOOK_ID = "1098907587631644692";
-const WEBHOOK_TOKEN =
-  "eAjGs6ahcUDpej_wEctrn_Fh7TuNoamSv65G20sIEl6eqoZPZh7sH3jE2dAmKTSO1-RS";
 export default function handler(req, res) {
   if (req.method === "POST") {
     // Process a POST request1
     console.log("request body :", req.body);
     console.log(req);
-    const path = `https://discordapp.com/api/webhooks/${WEBHOOK_ID}/${WEBHOOK_TOKEN}`;
+    const path = `https://discordapp.com/api/webhooks/${process.env.WEBHOOK_ID}/${process.env.WEBHOOK_TOKEN}`;
     axios
       .post(path, {
         content: `지역 : ${req.body.location} / NPC : ${req.body.npc} / 아이템 : ${req.body.item}`,
